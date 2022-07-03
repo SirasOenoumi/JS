52edb8711ffa71f7619a3d5e6e5215e38e807b61
SirasOenoumi <t_t_t_togashi0426@icloud.com>
2022-07-04 01:57:54 +0900

    イベント発火

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let lists = document.getElementsByClassName("pull-down-list")
-console.log(lists)
+
+
+window.addEventListener('load',function(){
+
+    const pullDownButton = document.getElementById("lists")
+    console.log(pullDownButton)
+})
+
+window.addEventListener('mouseover',function(){
+
+    const over =document.getElementById("lists")
+    console.log(over)
+})
+
+window.addEventListener('mouseout',function(){
+
+    const out =document.getElementById("lists")
+    console.log(out)
+})
