4f91f6dfe130d4118e294d4ec8af534de2159729
SirasOenoumi <t_t_t_togashi0426@icloud.com>
2022-07-11 02:22:25 +0900

    文字列の取得

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+function pullDown(){
 
-
-window.addEventListener('load',function(){
+ window.addEventListener('load',function(){
 
     const pullDownButton = document.getElementById("lists")
 
-})
+ })
 
-window.addEventListener('mouseover',function(){
+ window.addEventListener('mouseover',function(){
 
     const pullDownButton =document.getElementById("lists")
 
@@ -14,10 +14,10 @@ window.addEventListener('mouseover',function(){
     pullDownButton.addEventListener('mouseover',function(){
 
         this.setAttribute("style","background-color:blue;")
+  })
  })
-})
 
-window.addEventListener('mouseout',function(){
+ window.addEventListener('mouseout',function(){
 
     const pullDownButton =document.getElementById("lists")
 
@@ -27,17 +27,31 @@ window.addEventListener('mouseout',function(){
     })
 
 
-})
+ })
 
-window.addEventListener('click',function(){
+ window.addEventListener('click',function(){
 
     const pullDownButton = document.getElementById("lists")
     const pullDownParents = document.getElementById("pull-down")
+    const pullDownChild = document.querySelectorAll(".pull-down-list")
+    const currentList = document.getElementById("current-list")    
+
 
     pullDownButton.addEventListener('click',function(){
        pullDownParents.setAttribute("style" , "display:block;")
 
     })
 
+    pullDownChild.forEach(function(list){
+        list.addEventListener('click' , function() {
+            const value = list.innerHTML
+            currentList.innerHTML = value
+            console.log(value)
+        })
+    })
 
-})
+
+ })
+
+}
+window.addEventListener('load' , pullDown)
